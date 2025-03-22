011b8e85b240020185cf11c681d3c38054542ca7
AgungArf <ariefrosidinagung@gmail.com>
2025-03-21 20:24:36 -0700

    pertemuan 2

---
+++ hello-react/src/App.js
@@ -0,0 +1,23 @@
+// import Libraries
+
+import React from "react";
+import Header from "./Header.js";
+
+// membuat component dengan class
+
+
+// create Component
+
+const App =() =>{
+  return(
+  <div>
+    <h1>Hello React</h1>
+    <p>nama saya agung</p>
+    <Header/>
+  </div>
+  );
+};
+
+// export Default
+
+export default App;
